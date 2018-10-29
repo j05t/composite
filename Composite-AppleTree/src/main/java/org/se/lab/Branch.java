1065709158a824476b30ee59d023bef3bad0cc3f
total-recall <josef.steppan@gmail.com>
2018-10-29 11:21:00 +0100

    add decorator, composite

---
+++ Composite-AppleTree/src/main/java/org/se/lab/Branch.java
@@ -0,0 +1,61 @@
+package org.se.lab;
+
+import com.sun.nio.sctp.IllegalReceiveException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Branch extends Node
+{
+	/*
+	 * Composite operations
+	 */
+	
+	public int numberOfLeafs()
+	{
+		int number = 0;
+		for(Node n : getNodes())
+		{
+			number += n.numberOfLeafs();
+		}
+		return number;
+	}
+	
+	public int numberOfRedApples()
+	{
+		int number = 0;
+		for(Node n : getNodes())
+		{
+			number += n.numberOfRedApples();
+		}
+		return number;
+	}
+	
+	public int numberOfGreenApples()
+	{
+		int number = 0;
+		for(Node n : getNodes())
+		{
+			number += n.numberOfGreenApples();
+		}
+		return number;
+	}
+	
+	
+	/*
+	 * Navigation
+	 */
+	private List<Node> nodes = new ArrayList<Node>();
+	@Override
+	public void addNode(Node node)
+	{
+		if(node == null)
+			throw new IllegalArgumentException();
+		nodes.add(node);
+	}	
+	@Override
+	public List<Node> getNodes()
+	{
+		return nodes;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # patterns_exercises
-Exercises for Structural Design Patterns
+Exercises for Structural Design Patterns (import as Maven)
+1) Composite
+2) Decorator
+3) Bridge
